7c048235be1b98b88e7f8dc384b3c86dbd6d09de
cadedaley <126614302+cadedaley@users.noreply.github.com>
2023-07-17 10:28:59 -0500

    Update AD_Group_Copy_Function.ps1
    Changed wording of some functions for ease of understanding.
    
    Extended sleep timer.

--- Scripts/AD_Group_Copy_Function.ps1
+++ Scripts/AD_Group_Copy_Function.ps1
@@ -1,8 +1,8 @@
 ﻿import-Module ActiveDirectory
 
 #This Prompts the Administrator to enter the information located in the User Logon Name field
-$copy = Read-host "Enter the i1B Number of the User you wish to Copy Groups From"
-$paste = Read-host "Enter the i1B Number of the User you wish to Add Groups"
+$copy = Read-host "Enter the UserID of the User you wish to Copy Groups From"
+$paste = Read-host "Enter the UserID of the User you wish to Add Groups To"
 
 #This copys the users group from the 1st entry above and pastes those groups to the second entry entered
 $CopyFromUser = Get-ADUser $copy -prop MemberOf
@@ -10,7 +10,7 @@ $CopyToUser = Get-ADUser $paste -prop MemberOf
 
 $CopyFromUser.MemberOf | Where{$CopyToUser.MemberOf -notcontains $_} |  Add-ADGroupMember -Member $CopyToUser
 
-Write-Host "Import Completed! Closing in 5 Seconds." -ForegroundColor Green
+Write-Host "Import Completed! Closing in 10 Seconds." -ForegroundColor Green
 Write-Host "Please Re-Open the AD Window for the User to see Group Changes." -ForegroundColor Green
 
-Sleep 5
+Sleep 10

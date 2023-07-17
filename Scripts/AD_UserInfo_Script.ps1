8fdd0bb2176108f76456b7363db023f7ae9a204e
cadedaley <126614302+cadedaley@users.noreply.github.com>
2023-07-17 11:04:27 -0500

    Update AD_UserInfo_Script.ps1
    Changed Pathing to be Universal instead of tied to a single user.

--- Scripts/AD_UserInfo_Script.ps1
+++ Scripts/AD_UserInfo_Script.ps1
@@ -1,7 +1,7 @@
 ﻿#This script takes UserIDs from a text file and then outputs the information into a CSV File.
 #The following information is outputted: First Name, Last Name, Display Name, & Manager
 
-Get-Content -Path C:\Users\adm.cade.daley\Desktop\Users.txt |
+Get-Content -Path C:\Temp\Users.txt |
     ForEach-Object {
         Get-Aduser -Identity $_ -Properties 'GivenName', 'Surname', 'DisplayName', 'Manager' |
         Select-Object `
@@ -9,4 +9,4 @@ Get-Content -Path C:\Users\adm.cade.daley\Desktop\Users.txt |
             'Surname',
             'DisplayName',
             @{Name='Manager'; Expression={If ($_.Manager) {(Get-Aduser -Identity $_.Manager -Properties DisplayName).DisplayName} Else {''}}}
-    } | Export-Csv -Path C:\Users\adm.cade.daley\Desktop\UsersCompleted.csv -NoTypeIn
+    } | Export-Csv -Path C:\Temp\UsersCompleted.csv -NoTypeIn

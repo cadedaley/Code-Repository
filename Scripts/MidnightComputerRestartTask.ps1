8c7c3e71ab08099e4989d308091d41346db2d8fd
cadedaley <126614302+cadedaley@users.noreply.github.com>
2023-07-17 11:06:56 -0500

    Update MidnightComputerRestartTask.ps1
    Added Descriptive Comments

--- Scripts/MidnightComputerRestartTask.ps1
+++ Scripts/MidnightComputerRestartTask.ps1
@@ -1,5 +1,6 @@
-﻿$action = New-ScheduledTaskAction -Execute '%SystemRoot%\system32\shutdown.exe' -Argument '/r /f /t 0'
+# Simple script that adds a Scheduled Task to Restart the machine at midnight.
 
+$action = New-ScheduledTaskAction -Execute '%SystemRoot%\system32\shutdown.exe' -Argument '/r /f /t 0'
 $trigger = New-ScheduledTaskTrigger -Daily -At 12am
 
 Register-ScheduledTask -Action $action -Trigger $trigger -TaskName "Midnight Computer Restart" -Description "Restarts the Device every night at Midnight"

1f0664f7a332a8ebde54c66bcbd0fb5a693cd8b0
cadedaley <126614302+cadedaley@users.noreply.github.com>
2024-09-04 13:32:14 -0500

    Add files via upload

---
+++ Scripts/PowerShell/DownloadsFolderDelete.ps1
@@ -0,0 +1,10 @@
+﻿#Script to Delete Users Downloads folder.
+
+# Get the current user's username
+$username = $env:UserName
+
+# Get the current user's Downloads folder path
+$downloadsFolder = "C:\Users\$username\Downloads"
+
+# Delete everything in the Downloads folder
+Get-ChildItem $downloadsFolder -Force -Recurse | Remove-Item -Force -Recurse
